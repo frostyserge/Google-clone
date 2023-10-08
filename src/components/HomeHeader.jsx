97da9e6c417c29e9be362e4016e10b1c345a6563
frostyserge <eyemoroz@gmail.com>
2023-10-08 00:26:15 -0400

    finished styling the header section

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -4,8 +4,8 @@ import { TbGridDots } from "react-icons/tb";
 
 export default function HomeHeader() {
   return (
-    <header>
-        <div>
+    <header className="flex justify-end p-5 text-sm">
+        <div className="flex space-x-4 items-center">
             <Link href="https://mail.google.com" className="hover:underline">
                 Gmail
             </Link>
