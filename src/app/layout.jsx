b76e9b22708d89fe963a57657d7c39df93a496d4
frostyserge <eyemoroz@gmail.com>
2023-10-07 23:47:58 -0400

    previous commit message

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Google Clone',
+  description: 'Google Clone Next.js 13 - Sergii Moroz Version',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
