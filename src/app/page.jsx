bfc778d14668a7c9cda784e9c3c1918545c9fd49
frostyserge <eyemoroz@gmail.com>
2023-10-17 23:07:36 -0400

    initialized the body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
 import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+
+      {/* body */}
+
+      <div className="">
+          <Image 
+            width="300"
+            height="100"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          />
+      </div>
     </>
   )
 }
