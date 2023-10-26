93df99112949b5fc969fd3daa4472028ef9ae40c
frostyserge <eyemoroz@gmail.com>
2023-10-25 22:48:40 -0400

    search functionality set up

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,19 +1,33 @@
+"use client"
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiSolidMicrophone } from "react-icons/bi";
 
 export default function HomeSearch() {
+
+    const [input, setInput] = useState("");
+    const router = useRouter();
+    function handleSubmit (e) {
+        e.preventDefault();
+        if(!input.trim()) return;
+        router.push(`/search/web?searchTerm=${input}`)
+
+    }
+
   return (
     <>
-        <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-solid border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+        <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-solid border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
             <AiOutlineSearch className="text-xl text-gray-500" />
-            <input type="text" className="flex-grow focus:outline-none" />
+            <input onChange={(e) => setInput(e.target.value)} value={input} type="text" className="flex-grow focus:outline-none" />
             <BiSolidMicrophone className="text-lg" />
         </form>
-        <div>
-            <button>
+        <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
+            <button className="btn">
                 Google Search
             </button>
-            <button>
+            <button className="btn">
                 I'm Feeling Lucky!
             </button>
         </div>
