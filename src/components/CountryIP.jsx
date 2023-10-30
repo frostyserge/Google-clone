6834ac6a5cd0ddd6334560aa2298292c2b32c3d5
frostyserge <eyemoroz@gmail.com>
2023-10-29 21:30:23 -0400

    finished the footer. now we can see what country is the user from by implementing the extreme ip lookup API

--- src/components/CountryIP.jsx
+++ src/components/CountryIP.jsx
@@ -1,10 +1,14 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CountryIP() {
 
-  const [country, setCountry] = useState("United States")
+  const [country, setCountry] = useState("");
+  useEffect(() => {
+    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then((res) => res.json()).then((data) => setCountry(data.country))
+  }, [])
+  
 
   return (
     <div>
