8726f2b68543b43a36553d335b0d9646424e1320
frostyserge <eyemoroz@gmail.com>
2023-11-08 21:52:36 -0500

    positioned elememnts in the search header properly. added more styling.

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import SearchBox from "./SearchBox";
 import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOpt from './SearchHeaderOpt';
 
 export default function SearchHeader() {
     return (
-        <header>
-            <div className="">
+        <header className="sticky top-0 bg-white">
+            <div className="flex w-full p-6 items-center justify-between">
                 <Link href={'/'}>
                     <Image
                         width={'120'}
@@ -14,13 +16,16 @@ export default function SearchHeader() {
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
                     />
                 </Link>
-                <div>
+                <div className="flex-1">
                     <SearchBox />
                 </div>
-                <div>
-                <RiSettings3Line />
+                <div className="hidden md:inline-flex space-x-2 ">
+                    <RiSettings3Line className="header-icon" />
+                    <TbGridDots className="header-icon" />
                 </div>
+                <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2">Sign In</button>
             </div>
+            <SearchHeaderOpt />
         </header>
     );
 }
