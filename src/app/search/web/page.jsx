0202f0efcb5255bda2feb483e2818aa5e5bfa967
frostyserge <eyemoroz@gmail.com>
2023-11-12 22:22:45 -0500

    connected the google search engine api to the app

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,16 @@
 
-export default function WebSearchPage() {
+export default async function WebSearchPage() {
+
+  const SEARCH_API_KEY = process.env.SEARCH_API_KEY;
+  const CONTEXT_KEY = process.env.CONTEXT_KEY;
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${SEARCH_API_KEY}&cx=${CONTEXT_KEY}&q=lectures`)
+
+  const data = await response.json();
+  console.log(data);
+  const results = data.items;
   return (
-    <div>WebSearchPage</div>
+    <>
+      {results && results.map(result => <h1>{result.title}</h1>)}
+    </>
   )
 }
