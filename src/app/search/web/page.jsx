d4f69f142e19c0a56915aa4f0e828fca041853d1
frostyserge <eyemoroz@gmail.com>
2023-11-12 22:49:35 -0500

    configured error handling

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,13 @@
 
-export default async function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
 
   const SEARCH_API_KEY = process.env.SEARCH_API_KEY;
   const CONTEXT_KEY = process.env.CONTEXT_KEY;
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${SEARCH_API_KEY}&cx=${CONTEXT_KEY}&q=lectures`)
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${SEARCH_API_KEY}&cx=${CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+
+  if (!response.ok) { 
+    throw new Error("Oops...Something went wrong");
+  }
 
   const data = await response.json();
   console.log(data);
