12ed0ea970745eb1a8893b8a4ccea0473fb0d534
frostyserge <eyemoroz@gmail.com>
2023-11-15 23:30:59 -0500

    tweaked geolocation

--- src/components/CountryIP.jsx
+++ src/components/CountryIP.jsx
@@ -5,14 +5,17 @@ import { useEffect, useState } from "react";
 export default function CountryIP() {
 
   const [country, setCountry] = useState("");
+  const [city, setCity] = useState("")
+  const [region, setRegion] = useState("")
   useEffect(() => {
-    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then((res) => res.json()).then((data) => setCountry(data.country))
-  }, [])
-  
+    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then((res) => res.json()).then((data) => setCountry(data.country, data.city, data.region))
+    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then((res) => res.json()).then((data) => setCity(data.city))
+    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then((res) => res.json()).then((data) => setRegion(data.region))
+  }, []);
 
   return (
     <div>
-      {country}
+      {city}, {region}, {country}
     </div>
   )
 }
