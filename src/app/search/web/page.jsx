2a6cc010f9646e68a6576e56eb35b415477c4e1b
frostyserge <eyemoroz@gmail.com>
2023-11-19 20:24:08 -0500

    error handling tested and added to the project

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default async function WebSearchPage({searchParams}) {
 
@@ -10,11 +11,23 @@ export default async function WebSearchPage({searchParams}) {
   }
 
   const data = await response.json();
-  console.log(data);
+
   const results = data.items;
-  return (
-    <>
-      {results && results.map(result => <h1>{result.title}</h1>)}
-    </>
-  )
+
+  if(!results) {
+    return (
+    
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">Nor results found</h1>
+      <p className="text-lg">Try to search for something else or go back to homepage{" "}
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </p>
+    </div>
+
+    )
+  }
+    return <>{results && results.map(result => <h1>{result.title}</h1>)}</>
+
 }
